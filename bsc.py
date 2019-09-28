868a753560f160cf1bab0b28739f3a6105b8e6f5
Deepak-rathor <49443697+Deepak-rathor@users.noreply.github.com>
2019-09-28 23:58:31 +0530

    bsc

---
+++ bsc.py
@@ -0,0 +1,19 @@
+i=1
+
+while i<=5:
+    j = 1
+    while j<=5:
+
+        print("#", end=" ")
+        j=j+1
+    print("#")
+    i=i+1i=1
+print(===========)
+while i<=6:
+    j = 1
+    while j<=6:
+
+        print("#", end=" ")
+        j=j+1
+    print("#")
+    i=i+1
